5effdace04184b38ab8e641fe38156cef56ea31b
Anh Nguyen <94160753+AnhNguyenlost13@users.noreply.github.com>
2025-08-16 22:20:18 +0700

    beta: proper settings

--- src/Client/Module/Modules/ComboCounter/ComboCounter.cpp
+++ src/Client/Module/Modules/ComboCounter/ComboCounter.cpp
@@ -20,8 +20,8 @@ void ComboCounter::onDisable() {
 
 void ComboCounter::defaultConfig() {
     setDef("text", (std::string)"Combo: {value}");
-    Module::defaultConfig("all");
-    
+    Module::defaultConfig("core");
+    setDef("negatives", false);
 }
 
 void ComboCounter::settingsRender(float settingsOffset) {
@@ -52,6 +52,7 @@ void ComboCounter::settingsRender(float settingsOffset) {
 
     addHeader("Misc");
     defaultAddSettings("misc");
+    addToggle("Count to Negatives", "Allows the count to keep going down", "negatives")
 
     FlarialGUI::UnsetScrollView();
 
@@ -61,9 +62,19 @@ void ComboCounter::settingsRender(float settingsOffset) {
 
 void ComboCounter::onAttack(AttackEvent &event) {
     if (!this->isEnabled()) return;
-    if (std::chrono::high_resolution_clock::now() - last_hit > std::chrono::milliseconds(480)) {
+
+    auto now = std::chrono::high_resolution_clock::now();
+
+    if (Combo < 0) {
+        if (!getOps<bool>("negatives")) Combo = 0;
         Combo++;
-        last_hit = std::chrono::high_resolution_clock::now();
+        last_hit = now;
+        return;
+    }
+
+    if (now - last_hit > std::chrono::milliseconds(480)) {
+        Combo++;
+        last_hit = now;
     }
 }
 
@@ -74,16 +85,17 @@ void ComboCounter::onTick(TickEvent &event) {
 
     auto LP = reinterpret_cast<LocalPlayer*>(event.getActor());
 
-
     int currentHurtTime = LP->getHurtTime();
     if (currentHurtTime > 0 && lastHurtTime == 0) {
-        Combo--;
+        if (getOps<bool>("negatives")) Combo--;
+        else Combo = 0;
     }
     lastHurtTime = currentHurtTime;
 
     std::chrono::duration<double> duration = std::chrono::high_resolution_clock::now() - last_hit;
     if (duration.count() >= 15) Combo = 0;
-}
+
+
 
 void ComboCounter::onRender(RenderEvent &event) {
     if (this->isEnabled()) {
