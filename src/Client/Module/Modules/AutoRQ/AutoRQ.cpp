d4ff19c7940f51893b3b4ee32b787dd441d16016
Anh Nguyen <94160753+AnhNguyenlost13@users.noreply.github.com>
2025-09-01 21:08:03 +0700

    )

--- src/Client/Module/Modules/AutoRQ/AutoRQ.cpp
+++ src/Client/Module/Modules/AutoRQ/AutoRQ.cpp
@@ -373,7 +373,7 @@ void AutoRQ::reQ() {
 void AutoRQ::onKey(KeyEvent& event)
 {
     if (!this->isEnabled()) return;
-    FlarialGUI::Notify("Key ID " + event.getKey() + ", expected" + Utils::getStringAsKey(getOps<std::string>("bind"));
+    FlarialGUI::Notify("Key ID " + event.getKey() + ", expected" + Utils::getStringAsKey(getOps<std::string>("bind")));
     if (event.getKey() == Utils::getStringAsKey(getOps<std::string>("bind")) && static_cast<ActionType>(event.getAction()) == ActionType::Pressed) {
         reQ();
         FlarialGUI::Notify("Trying to requeue now");
